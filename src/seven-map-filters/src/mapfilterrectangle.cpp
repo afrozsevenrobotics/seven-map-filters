e1a5b78d223218ff28b3bff61d93055c13484254
afrozsevenrobotics <afroz.alam@sevenrobotics.in>
2023-08-24 21:27:41 +0530

    changed rectaangle point finding function

--- src/seven-map-filters/src/mapfilterrectangle.cpp
+++ src/seven-map-filters/src/mapfilterrectangle.cpp
@@ -8,19 +8,36 @@
 // }
 void transformRectPoint( geometry_msgs::Point rect_point, double rotation, std::vector<geometry_msgs::Point> &rectangle_points){
         
-        tf2::Vector3 v;
+     
         tf2::Quaternion q;
         q.setRPY(0,0,rotation);
         geometry_msgs::Quaternion g_q;
-        // geometry_msgs::VectorStamped vec(v);
-        // tf2::Vector3 v_new;
-        // v_new = tf2::quatRotate(q,v);
         g_q = tf2::toMsg(q);
         geometry_msgs::TransformStamped transform;
         transform.transform.rotation = g_q;
+        geometry_msgs::Vector3 origin_translation;
+        origin_translation.x = -2;
+        origin_translation.y = 0;
+        origin_translation.z = 0;
+        transform.transform.translation = origin_translation;
+
+        geometry_msgs::Point t_new_point;
         geometry_msgs::Point new_point;
         tf2::doTransform(rect_point,new_point,transform);
-        rectangle_points.push_back(new_point);
+        
+        geometry_msgs::TransformStamped rtransform;
+        origin_translation.x = 2;
+        origin_translation.y = 0;
+        origin_translation.z = 0;
+        rtransform.transform.translation = origin_translation;
+
+        q.setRPY(0,0,0);
+        g_q= tf2::toMsg(q);
+        rtransform.transform.rotation = g_q;
+        tf2::doTransform(new_point,t_new_point,rtransform);
+
+        std::cout<<t_new_point.x<<"  "<<t_new_point.y<<std::endl;
+        rectangle_points.push_back(t_new_point);
         
 
 }
@@ -38,20 +55,42 @@ void transformRectPoint( geometry_msgs::Point rect_point, double rotation, std::
     
 // }
 
-void updateRectangle(double length, double width, double rotation, geometry_msgs::Point rectangle_point,std::vector<geometry_msgs::Point>& rectangle_points){
+// void updateRectangle(double length, double width, double rotation, geometry_msgs::Point rectangle_point,std::vector<geometry_msgs::Point>& rectangle_points){
 
-    rect_p1 = rect_p2 = rect_p3 = rect_p4 = rectangle_point;
-    rect_p2.x = rectangle_point.x + length;
-    rect_p3.x = rectangle_point.x + length;
-    rect_p3.y = rectangle_point.y + width;
-    rect_p4.y = rectangle_point.y + width;
-    rectangle_points.push_back(rect_p1);
-    transformRectPoint(rect_p2,rotation,rectangle_points);
-    transformRectPoint(rect_p3,rotation,rectangle_points);
-    transformRectPoint(rect_p4,rotation,rectangle_points);
+//     rect_p1 = rect_p2 = rect_p3 = rect_p4 = rectangle_point;
+//     rect_p2.x = rectangle_point.x + length;
+//     rect_p3.x = rectangle_point.x + length;
+//     rect_p3.y = rectangle_point.y + width;
+//     rect_p4.y = rectangle_point.y + width;
+//     rectangle_points.push_back(rect_p1);
+//     transformRectPoint(rect_p2,rotation,rectangle_points);
+//     transformRectPoint(rect_p3,rotation,rectangle_points);
+//     transformRectPoint(rect_p4,rotation,rectangle_points);
     
-}
+// }
 
+void findRectanglePoints(double length, double width, double rotation, geometry_msgs::Point rectangle_point,double offset_x, double offset_y, std::vector<geometry_msgs::Point>& rectangle_points){
+       double cos_value = std::cos(rotation);
+       double sin_value = std::sin(rotation);
+       geometry_msgs::Point p1,p2, p3, p4;
+       p1 = rectangle_point;
+       p2.x = offset_x + rectangle_point.x + cos_value * length;
+       p2.y = offset_y + rectangle_point.y + sin_value * length;
+      
+      p3.x = offset_x + rectangle_point.x + (cos_value * length) - (sin_value * width);
+      p3.y = offset_y + rectangle_point.y + (sin_value * length) + (cos_value * width);
+
+      p4.x = offset_x + rectangle_point.x - (sin_value * width);
+      p4.y = offset_y + rectangle_point.y + (cos_value * width);
+
+      rectangle_points.push_back(p1);
+      rectangle_points.push_back(p2);
+      rectangle_points.push_back(p3);
+      rectangle_points.push_back(p4);
+
+
+
+}
 
 void getBresenhamLineCells(geometry_msgs::Point p1, geometry_msgs::Point p2, std::vector<geometry_msgs::Point>& pts) {
    //Bresenham Ray-Tracing
@@ -142,26 +181,26 @@ void findAllRectangleCell(std::vector<geometry_msgs::Point>lower_line_points, st
 int main(){
 
 geometry_msgs::Point p;
-p.x = -5.6;
-p.y = -0.4;
+p.x = -2;
+p.y = 0;
 p.z = 0;
-double length = 5.1;
-double width = 5.1;
+double length = 2;
+double width = 2;
 double rotation = 3.14; //rotation should be in radian
 std::vector<geometry_msgs::Point> rectangle_points;
 std::vector<geometry_msgs::Point> lower_line_points, upper_line_points;
 std::vector<geometry_msgs::Point> pts;
-updateRectangle(length,width,rotation,p,rectangle_points);
+// updateRectangle(length,width,rotation,p,rectangle_points);
+findRectanglePoints(5,5,1.57,p,0,0,rectangle_points);
 getBresenhamLineCells(rectangle_points[0],rectangle_points[1],lower_line_points);
 getBresenhamLineCells(rectangle_points[3],rectangle_points[2],upper_line_points);
 
-// std::cout<<"lower_line_points->"<<lower_line_points.size()<<std::endl;
-// std::cout<<"upper_line_points->"<<upper_line_points.size()<<std::endl;
-// std::cout<<p.x<<"    " << p.y<<std::endl;
-findAllRectangleCell(lower_line_points,upper_line_points,pts);
-    std::cout<<"printing value"<<pts.size()<<std::endl;
-for(int i = 0; i < pts.size(); i++){
-    std::cout << pts[i].x <<"  "<<pts[i].y<<std::endl;
+std::cout<<"printing all four point "<<rectangle_points.size()<<std::endl;
+for(int i = 0; i < rectangle_points.size(); i++){
+  std::cout<<rectangle_points[i].x<<"  "<<rectangle_points[i].y<<std::endl;
 }
+
+
+findAllRectangleCell(lower_line_points,upper_line_points,pts);
     return 0;
 }
